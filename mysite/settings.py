a89d93be34f2037a76861a17b18b42b3dd4a0e99
plradipawelec <rafal@radipawelec.pl>
2017-06-04 13:30:06 +0200

    Django folder2

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
