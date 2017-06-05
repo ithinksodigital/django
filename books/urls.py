42a1c840a93efce6193a6202e3aae178394d99cd
plradipawelec <rafal@radipawelec.pl>
2017-06-05 10:40:24 +0200

    Django folderx

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,6 @@ from books import views
 
 urlpatterns = [
 
-	url(r'^search-form/$', views.search_form),
-	url(r'^search/$', views.search)
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 ]
